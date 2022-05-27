12034160f8c68bf72998db1f679a3832d1f2e2fd
Ramsensei <ramsensei@estudiantec.cr>
2022-05-26 22:25:13 -0600

    figuras, paint fill

--- Matrix.hpp
+++ Matrix.hpp
@@ -18,29 +18,40 @@ public:
     void initializePixels();
     void printMatrix(Pixel ***matrix, bool traspose = false);
     Pixel ***getMatrix();
+    void setColor(Pixel *pixel, CurrentColor color);
+    Pixel *getPixel(int i, int j);
     int getHeight();
     int getWidth();
 
     void pencil(CurrentColor color, int i, int j, int line_width);
     void pen(CurrentColor color, int x1, int y1, int x2, int y2, int line_width);
-    void setColor(Pixel *pixel, CurrentColor color);
-    Pixel *getPixel(int i, int j);
+    void eraser(int i, int j, int eraser_width);
+
     void grayScaleFilter();
     void negativeFilter();
     void ramFilter();
     void sepiaFilter();
+
     void traspose();
     void flipX();
     void flipY();
-    void polygon(CurrentColor color, int x, int y, int l, int n, int line_width);
     void rotate();
+
+    void BFS(CurrentColor picked_color, CurrentColor new_color, int x, int y);
+    void paintFill(CurrentColor picked_color, CurrentColor new_color, int x, int y);
+    void setMemoryFlag(int flag);
+
+    void polygon(CurrentColor color, int x, int y, int l, int n, int line_width);
+    void rectangle(CurrentColor color, int x, int y, int b, int h, int line_width);
+    void circle(CurrentColor color, int h, int k, int r, int line_width);
+    
     void deleteMatrix(Pixel ***temp_matrix);
     void switchDimentions();
-    void eraser(int i, int j, int eraser_width);
 
 private:
     int height, width;
     Pixel ***matrix;
+    int memoryFlag;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@
 
 int main()
 {
-    Matrix *matrix = new Matrix(1000, 1000);
+    Matrix *matrix = new Matrix(500, 500);
 
     // Picking color
     CurrentColor purple = CurrentColor(78, 50, 168);
     CurrentColor red = CurrentColor(204, 10, 10);
+    CurrentColor blue = CurrentColor(0, 0, 254);
+    CurrentColor white = CurrentColor(255, 255, 255);
+    CurrentColor black = CurrentColor(0, 0, 0);
 
-    // matrix->pen(purple, 0, 24, 49, 0, 2);
-    // matrix->pen(red, 0, 0, 25, 25, 3);
-    // matrix->pencil(purple, 5, 5, 3);
-    matrix->polygon(purple, 5, 5, 200, 8, 1);
+    matrix->circle(purple, 250, 250, 50, 2);
+    matrix->circle(purple, 270, 270, 50, 2);
+
+
+    matrix->paintFill(white, red, 210, 250);
+    matrix->paintFill(white, black, 250, 250);
+    matrix->paintFill(white, blue, 310, 270);
+    matrix->pencil(red, 310, 270, 2);
+
     Bitmap *image_test = new Bitmap(matrix->getMatrix(), matrix->getHeight(), matrix->getWidth(), "original.bmp");
+    
     matrix->rotate();
     Bitmap *image_test2 = new Bitmap(matrix->getMatrix(), matrix->getHeight(), matrix->getWidth(), "rotate.bmp");
 }
